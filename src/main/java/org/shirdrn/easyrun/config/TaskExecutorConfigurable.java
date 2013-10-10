5651336c036c3900233573ca1bf15195b319df13
Yanjun <shiyanjun@yeah.net>
2013-10-10 17:45:33 +0800

    Update interface 'TaskExecutorConfigurable' to extend interface 'Breakable'.

--- src/main/java/org/shirdrn/easyrun/config/TaskExecutorConfigurable.java
+++ src/main/java/org/shirdrn/easyrun/config/TaskExecutorConfigurable.java
@@ -1,10 +1,11 @@
 package org.shirdrn.easyrun.config;
 
+import org.shirdrn.easyrun.common.Breakable;
 
-public interface TaskExecutorConfigurable {
+
+public interface TaskExecutorConfigurable extends Breakable {
 
 	TaskExecutorChainBuilder terminateWhenFailure();
 	TaskExecutorChainBuilder terminateWhenFailure(boolean terminate);
-	boolean isTerminateWhenFailure();
 	
 }
