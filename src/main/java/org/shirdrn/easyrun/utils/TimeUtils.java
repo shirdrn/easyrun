d9d5e7323efebfc31abb5dfcc42c00adcbc31cd0
Yanjun <shiyanjun@yeah.net>
2013-10-23 14:08:21 +0800

    Add utility method to compute days between 2 dates.

--- src/main/java/org/shirdrn/easyrun/utils/TimeUtils.java
+++ src/main/java/org/shirdrn/easyrun/utils/TimeUtils.java
@@ -38,4 +38,32 @@ public class TimeUtils {
 		DateFormat df = new SimpleDateFormat(format);
 		return df.format(new Date(date));
 	}
+	
+	public static String[] between(String start, String end, String format) {
+		DateFormat df = new SimpleDateFormat(format);
+		int before1 = whichDayBefore(start, format);
+		int before2 = whichDayBefore(end, format);
+		String[] days = new String[Math.abs(before1) - Math.abs(before2) + 1];
+		for(int i=before1, j=0; i<=before2; i++, j++) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.DAY_OF_MONTH, i);
+			String temp = df.format(calendar.getTime());
+			days[j] = temp;
+		}
+		return days;
+	}
+	
+	private static int whichDayBefore(String date, String format) {
+		DateFormat df = new SimpleDateFormat(format);
+		int which = 0;
+		while(true) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.DAY_OF_MONTH, which);
+			String temp = df.format(calendar.getTime());
+			if(temp.equals(date)) {
+				return which;
+			}
+			which--;
+		}
+	}
 }
